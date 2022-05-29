9d4e83407932cbd215e63e385d68b741d45d77ab
bummy <dydqja4582@naver.com>
2022-05-30 00:09:48 +0900

    220529

---
+++ codeup1002.java
@@ -0,0 +1,7 @@
+import com.oracle.xmlns.internal.webservices.jaxws_databinding.SoapBindingParameterStyle;
+
+public class codeup1002 {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
